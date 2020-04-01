49d97a36c43b5b370c1699f39cf7df3f691c0e46
Carlota Vina Sirgo <carlota_vina@hotmail.com>
2020-04-01 18:42:55 +0200

    Creating a Child Component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,27 @@
+<template>
+<ul>
+ <li v-for="book in books" :key="book.title">
+    {{book.title}}:{{book.author}}
+</li>
+</ul>
+    
+</template>
+<script>
+export default {
+    name: "BookItem",
+    props: ["book"]    
+}
+</script>
+   
+
+<style>
+ ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: block;
+        margin: 0 10px;
+    }
+    
+</style>
